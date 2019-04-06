050defec82108a330c4a9385e26ce672830eb3b1
LuckyJustCoder <lakujust@gmail.com>
2019-04-07 00:40:21 +0300

    что-то работает

--- Main.py
+++ Main.py
@@ -38,17 +38,24 @@ class Apps(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
 
 def main():
-    print(utils.bordered("Information", " Run the program with arguments: {0}".format(sys.argv)))
+
     if len(sys.argv) > 1:
         path = "null"
+        mode = "test"
         for i in range(len(sys.argv)):
-            if i == "-o":
+            if sys.argv[i] == "-o":
                 if i + 1 < len(sys.argv):
                     path = sys.argv[i + 1]
-        if sys.argv[1] == "-train":
+            if sys.argv[i] == "-train":
+                mode = "train"
+
+        print(utils.bordered("Information", " Run the program with arguments: {0},\n Path: {1}, Mode: {2}".format(sys.argv, path,mode)))
+        if mode == "train":
             environment = Environment()
             if path != "null":
-                environment = pickle.load(path)
+                f = open(path, 'rb')
+                environment = pickle.load(f)
+                f.close()
             else:
                 environment.setup()
             while True:
@@ -58,11 +65,7 @@ def main():
             window = Apps()
             window.show()
             app.exec_()
-    else:
-        app = QtWidgets.QApplication(sys.argv)
-        window = Apps()
-        window.show()
-        app.exec_()
+
 
 
 if __name__ == '__main__':
