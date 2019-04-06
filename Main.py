b8367916fc47f7bcd142051504283d9d670dfbf3
LuckyJustCoder <lakujust@gmail.com>
2019-04-07 00:24:50 +0300

    что-то работает

--- Main.py
+++ Main.py
@@ -14,7 +14,16 @@ class Apps(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.environment = Environment()  # Инициализирую среду
-        self.environment.setup()  # Произвожу первоначальную настройку
+        path = "null"
+        for i in range(len(sys.argv)):
+            if i == "-o":
+                if i + 1 < len(sys.argv):
+                    path = sys.argv[i + 1]
+        if path != "null":
+            self.environment = pickle.load(path)
+        else:
+            self.environment.setup()  # Произвожу первоначальную настройку
+
         self.setupUi(self)  # Инициализирую gui
         t1 = threading.Thread(target=self.__update)
         t1.start()
@@ -31,13 +40,20 @@ class Apps(QtWidgets.QMainWindow, design.Ui_MainWindow):
 def main():
     print(utils.bordered("Information", " Run the program with arguments: {0}".format(sys.argv)))
     if len(sys.argv) > 1:
+        path = "null"
+        for i in range(len(sys.argv)):
+            if i == "-o":
+                if i + 1 < len(sys.argv):
+                    path = sys.argv[i + 1]
         if sys.argv[1] == "-train":
             environment = Environment()
-            environment.setup()
+            if path != "null":
+                environment = pickle.load(path)
+            else:
+                environment.setup()
             while True:
                 environment.update()
-
-        if sys.argv[1] == "-test":
+        else:
             app = QtWidgets.QApplication(sys.argv)
             window = Apps()
             window.show()
@@ -47,7 +63,6 @@ def main():
         window = Apps()
         window.show()
         app.exec_()
-
 
 
 if __name__ == '__main__':
