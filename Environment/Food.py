b4e0a211baea84f4043a0b17032f9efeac707297
LuckyJustCoder <lakujust@gmail.com>
2019-04-07 01:35:28 +0300

    что-то работает

--- Environment/Food.py
+++
@@ -1,8 +0,0 @@
-class Food:
-    def __init__(self, x, y, radius, color, energy):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-        self.energy = energy
-        self.is_alive = True
